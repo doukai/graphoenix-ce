fd0b61b441a43db255967ad25e8fb52098d82933
doukai <doukai.java@gmail.com>
2023-12-16 17:53:25 +0800

    update

--- graphoenix-spi/src/main/java/io/graphoenix/spi/graphql/Definition.java
+++ graphoenix-spi/src/main/java/io/graphoenix/spi/graphql/Definition.java
@@ -34,6 +34,33 @@ public interface Definition {
         throw new RuntimeException("unsupported document definition: " + definitionContext.getText());
     }
 
+    static Definition merge(GraphqlParser.DefinitionContext definitionContext) {
+        if (definitionContext.operationDefinition() != null) {
+            return new Operation(definitionContext.operationDefinition());
+        } else if (definitionContext.fragmentDefinition() != null) {
+            return new FragmentDefinition(definitionContext.fragmentDefinition());
+        } else if (definitionContext.typeSystemDefinition() != null) {
+            if (definitionContext.typeSystemDefinition().schemaDefinition() != null) {
+                return new Schema(definitionContext.typeSystemDefinition().schemaDefinition());
+            } else if (definitionContext.typeSystemDefinition().typeDefinition() != null) {
+                if (definitionContext.typeSystemDefinition().typeDefinition().scalarTypeDefinition() != null) {
+                    return new ScalarType(definitionContext.typeSystemDefinition().typeDefinition().scalarTypeDefinition());
+                } else if (definitionContext.typeSystemDefinition().typeDefinition().enumTypeDefinition() != null) {
+                    return new EnumType(definitionContext.typeSystemDefinition().typeDefinition().enumTypeDefinition());
+                } else if (definitionContext.typeSystemDefinition().typeDefinition().objectTypeDefinition() != null) {
+                    return new ObjectType(definitionContext.typeSystemDefinition().typeDefinition().objectTypeDefinition());
+                } else if (definitionContext.typeSystemDefinition().typeDefinition().interfaceTypeDefinition() != null) {
+                    return new InterfaceType(definitionContext.typeSystemDefinition().typeDefinition().interfaceTypeDefinition());
+                } else if (definitionContext.typeSystemDefinition().typeDefinition().inputObjectTypeDefinition() != null) {
+                    return new InputObjectType(definitionContext.typeSystemDefinition().typeDefinition().inputObjectTypeDefinition());
+                }
+            } else if (definitionContext.typeSystemDefinition().directiveDefinition() != null) {
+                return new DirectiveDefinition(definitionContext.typeSystemDefinition().directiveDefinition());
+            }
+        }
+        throw new RuntimeException("unsupported document definition: " + definitionContext.getText());
+    }
+
     default boolean isSchema() {
         return false;
     }
--- graphoenix-spi/src/main/java/io/graphoenix/spi/graphql/Document.java
+++ graphoenix-spi/src/main/java/io/graphoenix/spi/graphql/Document.java
@@ -1,19 +1,18 @@
 package io.graphoenix.spi.graphql;
 
 import graphql.parser.antlr.GraphqlParser;
-import io.graphoenix.spi.graphql.operation.FragmentDefinition;
-import io.graphoenix.spi.graphql.operation.Operation;
-import io.graphoenix.spi.graphql.type.*;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.util.Collection;
-import java.util.LinkedHashSet;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static io.graphoenix.spi.utils.DocumentUtil.graphqlToDocument;
 
@@ -25,6 +24,9 @@ public class Document {
 
     public Collection<Definition> getDefinitions() {
         return definitions;
+    }
+
+    public Document() {
     }
 
     public Document(String graphql) {
@@ -44,41 +46,130 @@ public class Document {
     }
 
     public Document addDefinitions(String graphql) {
-        if (this.definitions == null) {
-            this.definitions = new LinkedHashSet<>();
-        }
-        this.definitions.addAll(
+        return addDefinitions(
                 graphqlToDocument(graphql).definition().stream()
                         .map(Definition::of)
                         .collect(Collectors.toList())
         );
-        return this;
     }
 
-    public Document addDefinitions(InputStream inputStream) {
-        if (this.definitions == null) {
-            this.definitions = new LinkedHashSet<>();
-        }
-        this.definitions.addAll(
-                graphqlToDocument(graphql).definition().stream()
+    public Document addDefinitions(InputStream inputStream) throws IOException {
+        return addDefinitions(
+                graphqlToDocument(inputStream).definition().stream()
                         .map(Definition::of)
                         .collect(Collectors.toList())
         );
-        return this;
     }
 
-    public Document addDefinitions(String graphql) {
-        if (this.definitions == null) {
-            this.definitions = new LinkedHashSet<>();
-        }
-        this.definitions.addAll(
-                graphqlToDocument(graphql).definition().stream()
+    public Document addDefinitions(File file) throws IOException {
+        return addDefinitions(
+                graphqlToDocument(file).definition().stream()
                         .map(Definition::of)
                         .collect(Collectors.toList())
         );
+    }
+
+    public Document addDefinitions(Path path) throws IOException {
+        return addDefinitions(
+                graphqlToDocument(path).definition().stream()
+                        .map(Definition::of)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public Document addDefinitionsByFileName(String fileName) throws IOException {
+        return addDefinitionsByFileName(fileName, this.getClass().getClassLoader());
+    }
+
+    public Document addDefinitionsByFileName(String fileName, ClassLoader classLoader) throws IOException {
+        if (Files.exists(Path.of(fileName))) {
+            return addDefinitions(new File(fileName));
+        } else {
+            try {
+                InputStream inputStream = classLoader.getResourceAsStream(fileName);
+                if (inputStream != null) {
+                    return addDefinitions(inputStream);
+                }
+            } catch (FileSystemNotFoundException fileSystemNotFoundException) {
+                Map<String, String> env = new HashMap<>();
+                URL resource = classLoader.getResource(fileName);
+                try (FileSystem fileSystem = FileSystems.newFileSystem(Objects.requireNonNull(resource).toURI(), env)) {
+                    return addDefinitions(fileSystem.getPath(fileName));
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
         return this;
     }
 
+    public Document addDefinitionsByPathName(String pathName) throws IOException {
+        return addDefinitionsByPathName(pathName, this.getClass().getClassLoader());
+    }
+
+    public Document addDefinitionsByPathName(String pathName, ClassLoader classLoader) throws IOException {
+        Path path = Path.of(pathName);
+        if (Files.exists(path)) {
+            return addDefinitions(path);
+        } else {
+            URL resource = classLoader.getResource(pathName);
+            if (resource != null) {
+                try {
+                    Path resourcePath = Paths.get(resource.toURI());
+                    if (Files.exists(resourcePath)) {
+                        try (Stream<Path> pathStream = Files.list(resourcePath)) {
+                            List<Path> filePathList = pathStream.collect(Collectors.toList());
+                            for (Path filePath : filePathList) {
+                                addDefinitions(filePath);
+                            }
+                            return this;
+                        }
+                    }
+                } catch (FileSystemNotFoundException fileSystemNotFoundException) {
+                    Map<String, String> env = new HashMap<>();
+                    try (FileSystem fileSystem = FileSystems.newFileSystem(Objects.requireNonNull(resource).toURI(), env);
+                         Stream<Path> pathStream = Files.list(fileSystem.getPath(pathName))) {
+                        List<Path> filePathList = pathStream.collect(Collectors.toList());
+                        for (Path filePath : filePathList) {
+                            addDefinitions(filePath);
+                        }
+                        return this;
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return this;
+    }
+
+    public Document addDefinitionsByPathName(String pathName, String resourcePath) throws IOException {
+        Path graphqlPath = Paths.get(resourcePath).resolve(pathName);
+        try {
+            if (Files.exists(graphqlPath)) {
+                try (Stream<Path> pathStream = Files.list(graphqlPath)) {
+                    List<Path> filePathList = pathStream.collect(Collectors.toList());
+                    for (Path filePath : filePathList) {
+                        addDefinitions(filePath);
+                    }
+                    return this;
+                }
+            }
+        } catch (FileSystemNotFoundException fileSystemNotFoundException) {
+            Map<String, String> env = new HashMap<>();
+            try (FileSystem fileSystem = FileSystems.newFileSystem(graphqlPath.toUri(), env);
+                 Stream<Path> pathStream = Files.list(fileSystem.getPath(pathName))) {
+                List<Path> filePathList = pathStream.collect(Collectors.toList());
+                for (Path filePath : filePathList) {
+                    addDefinitions(filePath);
+                }
+                return this;
+            }
+        }
+        return this;
+    }
 
     public Document(GraphqlParser.DocumentContext documentContext) {
         this.definitions = documentContext.definition().stream()
