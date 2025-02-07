19b0d75905b868bc5d395b4e9b59c62148d74f83
doukai <doukai.java@gmail.com>
2025-02-07 17:08:22 +0800

    Update OperationDAO.java

--- graphoenix-spi/src/main/java/io/graphoenix/spi/dao/OperationDAO.java
+++ graphoenix-spi/src/main/java/io/graphoenix/spi/dao/OperationDAO.java
@@ -1,18 +1,24 @@
 package io.graphoenix.spi.dao;
 
+import io.nozdormu.spi.async.Async;
+import io.nozdormu.spi.async.Asyncable;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Type;
 import java.util.Map;
 
-public interface OperationDAO {
+public interface OperationDAO extends Asyncable {
 
+    @Async
     <T> T find(String sql, Map<String, Object> parameters, Class<T> beanClass);
 
+    @Async
     <T> T find(String sql, Map<String, Object> parameters, Type type);
 
+    @Async
     <T> T save(String sql, Map<String, Object> parameters, Class<T> beanClass);
 
+    @Async
     <T> T save(String sql, Map<String, Object> parameters, Type type);
 
     <T> Mono<T> findAsync(String sql, Map<String, Object> parameters, Class<T> beanClass);
